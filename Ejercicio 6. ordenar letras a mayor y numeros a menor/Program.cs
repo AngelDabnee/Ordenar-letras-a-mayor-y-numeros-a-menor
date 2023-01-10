3f8b4256e8a741b14b3de17a9707ba5be0c68f79
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-09 19:05:26 -0700

    first commit

---
+++ Ejercicio 6. ordenar letras a mayor y numeros a menor/Program.cs
@@ -0,0 +1,15 @@
+﻿// See/* https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Mediante un ciclo, pedir 10 letras y 10 números, ordenar ascendentemente las letras,
+//descendentemente los números. Imprimirlos.
+Console.WriteLine("En este programa ordenaremos 10 letras de mayor a menor y los números de menor a mayor");
+//De
+char[] letras = new char[10];
+double[] numeros = new double[10];
+//Proceso
+for (int i = 0; i < letras.Length; i++)
+{
+    Console.WriteLine("Captura 10 letras, letra No. " + (i + 1));
+    letras[i] = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine(Convert.ToInt32(letras));
+}
